3757132c9f8dd62cfef67d03aca67b71e3473f21
semvlu <74308718+semvlu@users.noreply.github.com>
2023-12-01 16:00:34 +0800

    Add files via upload

---
+++ Quiz9/IRollable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uB10933001_Mid
+{
+    interface IRollable
+    {
+        double Distance();
+        string RollShapeProperty();
+    }
+}
